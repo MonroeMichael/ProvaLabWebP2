9e207df8c8ad436f3647e650bb7d56b15c3664ee
MonroeMichael <144135768+MonroeMichael@users.noreply.github.com>
2023-11-21 20:20:03 -0300

    js.js

--- js.js
+++ js.js
@@ -1,4 +1,16 @@
+window.onload = function() {
+    document.getElementById('mascara').style.display = 'block';
+    document.getElementById('popup').style.display = 'block';
+}
 
+document.getElementById('aceitar').onclick = function() {
+    document.getElementById('mascara').style.display = 'none';
+    document.getElementById('popup').style.display = 'none';
+}
+
+document.getElementById('recusar').onclick = function() {
+    location.reload();
+}
 
 window.dataLayer = window.dataLayer || [];
 function gtag() { dataLayer.push(arguments); }
